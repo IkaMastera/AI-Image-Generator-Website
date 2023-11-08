01373bc14469c3f80ce65ea91ded46c6362c3794
Irakli <112602982+IkaMastera@users.noreply.github.com>
2023-11-08 14:03:44 +0400

    finish: create post component
    added some logic for the handleSurpriseMe funciton and handleChange function, fixed the misspell on the prompt and some visual aspect of the createPost component as well

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -9,18 +9,25 @@ const CreatePost = () => {
   const navigate = useNavigate();
   const [form, setForm] = useState({
     name: "",
-    promp: "",
+    prompt: "",
     photo: "",
   });
 
   const [generatingImg, setGeneratingImg] = useState(false);
   const [loading, setLoading] = useState(false);
 
+  const generateImage = () => {}
+
   const handleSubmit = () => {}
 
-  const handleChange = (e) => {}
+  const handleChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value })
+  }
 
-  const handleSurpriseMe = () => {}
+  const handleSurpriseMe = () => {
+    const randomPrompt = getRandomPrompt(form.prompt);
+    setForm({...form, prompt: randomPrompt})
+  }
 
   return (
    <section className='max-w-7x mx-auto'>
@@ -53,6 +60,50 @@ const CreatePost = () => {
             isSurpriseMe
             handleSurpriseMe = {handleSurpriseMe}
            />
+
+           <div className='relative bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-64 p-3 h-64 flex justify-center items-center'>
+               {form.photo ? (
+                <img 
+                src={form.photo}
+                alt={form.prompt} 
+                className='w-full h-full object-contain' 
+                />
+               ): (
+                <img 
+                src={preview}
+                alt='preview'
+                className='w-9/12 h-9/12 object-contain opacity-40'
+                />
+               )}
+
+               {generatingImg && (
+                <div className='absolute inset-0 z-0 flex justify-center items-center bg-[rgba(0,0,0,0.5)] rounded-lg'>
+                  <Loader/>
+                </div>
+               )}
+           </div>
+        </div>
+
+        <div className='mt-5 flex gap-5'>
+           <button 
+           type = "button"
+           onClick={generateImage}
+           className='text-white bg-green-700 font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center'
+           >
+             {generatingImg ? 'Generating...' : 'Generate'}
+           </button>
+        </div>
+
+        <div className='mt-10'>
+           <p className='mt-2 text-[#666e75] text-[14px]'>
+            Once you have Created the image if you want you can share them with other in Community
+           </p>
+           <button
+           type="submit"
+           className='mt-3 text-white bg-[#6469ff] font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center'
+           >
+            {loading ? 'Sharing...' : "Share with the community"}
+           </button>
         </div>
       </form>
    </section>
